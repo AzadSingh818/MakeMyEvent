4941080b6251afa65461e5bba57428d412395be1
Azad Singh <azad818n.s@gmail.com>
2025-09-11 02:01:56 +0530

    feat: add Checkbox component export to UI index file

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -6,6 +6,7 @@ export { Button, buttonVariants } from './button'
 export { Input } from './input'
 export { Label } from './label'
 export { Textarea } from './textarea'
+export { Checkbox } from './checkbox'
 export { 
   Select,
   SelectContent,
